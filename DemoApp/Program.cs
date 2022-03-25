ef950fc16c076ea863335e1ecddbd109e314d5ea
Roger Johansson <roger.johansson@charge-amps.com>
2022-03-25 14:32:11 +0100

    .

---
+++ DemoApp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TinyTypes;
+
+Console.WriteLine("Hello, World!");
+
+IO<int> stuff = async () =>
+{
+    await Task.Delay(1000);
+    throw new ArgumentException("Foo!");
+    Console.WriteLine("Hello");
+    return 1;
+};
+
+var x = await stuff.Try()();
+
+switch (x)
+{
+    case Some<int>(var i):
+        Console.WriteLine("Got Int " + i);
+        break;
+    case Some<Exception>(var ex):
+        Console.WriteLine("Got exception " + ex);
+        break;
+}
